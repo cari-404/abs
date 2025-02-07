947d3f5fd71f5ad1f3c50028a4ee65f51159367f
cari-404 <buatagus01@gmail.com>
2025-02-08 00:59:08 +0700

    add build windows XP action

--- launcabs/buildXP.rs
+++ launcabs/buildXP.rs
@@ -1,8 +1,12 @@
 extern crate winresource;
 fn main() {
     if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
+		let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+		if target_arch == "x86" || target_arch == "x86_64" {
+			thunk::thunk();
+		}
         let mut res = winresource::WindowsResource::new();
-        res.set_icon("cart.ico");
+        res.set_icon("launcher.ico");
 		res.set_manifest(r#"
 		<?xml version="1.0" encoding="UTF-8" standalone="yes"?> 
 		<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
@@ -14,6 +18,7 @@ fn main() {
 			</dependency>
 		</assembly>
 		"#);
+		res.set_version_info(winresource::VersionInfo::FILEVERSION, 0x0000000900090001);
         res.compile().unwrap();
     }
 }
--- savevoucher/buildXP.rs
+++ savevoucher/buildXP.rs
@@ -18,6 +18,10 @@ fn main() {
 }
 #[cfg(target_env = "msvc")]
 fn main() {
+    let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    if target_arch == "x86" || target_arch == "x86_64" {
+        thunk::thunk();
+    }
     let res_path = env::current_dir()
         .unwrap()
         .join("resources")
