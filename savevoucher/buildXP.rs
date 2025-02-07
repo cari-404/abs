2db1bd23cceca7e2e8c35e16de3913d1b31dc09e
cari-404 <buatagus01@gmail.com>
2025-02-08 01:14:45 +0700

    fix build.rs savevoucher

--- savevoucher/buildXP.rs
+++ savevoucher/buildXP.rs
@@ -1,34 +1,22 @@
-use std::env;
 extern crate winresource;
-
-#[cfg(target_env = "gnu")]
 fn main() {
     if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
-        let res_path = env::current_dir()
-            .unwrap()
-            .join("resources")
-            .join("resources_coff.res");
-
-        println!("cargo:rerun-if-changed={}", res_path.display());
-        println!("cargo:rustc-link-arg={}", res_path.display());
+		let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+		if target_arch == "x86" || target_arch == "x86_64" {
+			thunk::thunk();
+		}
         let mut res = winresource::WindowsResource::new();
-        res.set_version_info(winresource::VersionInfo::FILEVERSION, 0x0000000A000A0001);
+		res.set_manifest(r#"
+		<?xml version="1.0" encoding="UTF-8" standalone="yes"?> 
+		<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+			<description>native-windows-gui comctl32 manifest</description> 
+			<dependency>
+				<dependentAssembly>
+					<assemblyIdentity type="win32" name="Microsoft.Windows.Common-Controls" version="6.0.0.0" processorArchitecture="*" publicKeyToken="6595b64144ccf1df" language="*" /> 
+				</dependentAssembly>
+			</dependency>
+		</assembly>
+		"#);
         res.compile().unwrap();
     }
-}
-#[cfg(target_env = "msvc")]
-fn main() {
-    let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
-    if target_arch == "x86" || target_arch == "x86_64" {
-        thunk::thunk();
-    }
-    let res_path = env::current_dir()
-        .unwrap()
-        .join("resources")
-        .join("resources.res");
-
-    println!("cargo:rerun-if-changed={}", res_path.display());
-    println!("cargo:rustc-link-arg={}", res_path.display());
-    let mut res = winresource::WindowsResource::new();
-    res.compile().unwrap();
 }
